4c71d265e8d9b05fb5254d90d3cca8e6fe993a45
Pushkina Olga <archillas@mail.ru>
2024-03-11 00:36:44 +0300

    task completed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,11 @@
 package com.epam.mjc;
 
+import java.util.stream.Collectors;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x ->  x.stream().map(each -> each/divider).collect(Collectors.toList());
+
     }
 }
